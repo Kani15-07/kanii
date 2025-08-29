585cb71f5a27a2c9c1c7fe5cc78ea0e946524360
Kanika <kanika.26ec@licet.ac.in>
2025-08-29 12:39:31 +0530

    hi

---
+++ src/main/java/com/kani/kanika/KanikaApplication.java
@@ -0,0 +1,13 @@
+package com.kani.kanika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kani/kanika/KanikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kani.kanika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
